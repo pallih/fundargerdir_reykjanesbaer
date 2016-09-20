b87949b62e2eb119946c72eca1a8733c4fd2e3b4
Páll Hilmarsson <pallih@kaninka.net>
2016-09-20 22:08:40 +0000

    initial commit

--- scraper.py
+++ scraper.py
@@ -1,24 +1,58 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+# -*- coding: utf-8 -*-
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+import scraperwiki
+import lxml.html
+import requests
+import urlparse
+from dateutil import parser
+from dateutil.parser import parserinfo
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+BASE_URL = "http://www.reykjanesbaer.is"
+DATA_URL = "http://www.reykjanesbaer.is/stjornkerfi/fundargerdir?page=1&group_id=&datefrom=&dateto=&keywords="
+
+
+class Icelandic(parserinfo):
+    def __init__(self):
+        self.WEEKDAYS = [(u"Mán", u"Mánudagur"),
+                         (u"Þri", u"Þriðjudagur"),
+                         (u"Mið", u"Miðvikudagur"),
+                         (u"Fim", u"Fimmtudagur"),
+                         (u"Fös", u"Föstudagur"),
+                         (u"Lau", u"Laugardagur"),
+                         (u"Sun", u"Sunnudagur")]
+        self.MONTHS = [(u"Jan", u"janúar"),
+                       (u"Feb", u"febrúar"),
+                       (u"Mar", u"mars"),
+                       (u"Apr", u"apríl"),
+                       (u"maí", u"maí"),
+                       (u"jún", u"júní"),
+                       (u"júl", u"júlí"),
+                       (u"ágú", u"ágúst"),
+                       (u"sep", u"september"),
+                       (u"okt", u"október"),
+                       (u"nov", u"nóvember"),
+                       (u"des", u"desember")]
+        parserinfo.__init__(self)
+
+    def __call__(self):
+        return self
+
+
+icelandic_dateutil_parserinfo = Icelandic()
+
+
+r = requests.get(DATA_URL)
+root = lxml.html.fromstring(r.text)
+trs = root.xpath("//table[@id='fundagerd']/tbody/tr")
+data = []
+for tr in trs:
+    meeting = {}
+    meeting["nefnd"] = tr[0].text
+    meeting["titill"] = tr[1][0].text
+    meeting["url"] = urlparse.urljoin(BASE_URL, tr[1][0].attrib["href"])
+    meeting["dagsetning"] = tr[2].text
+    meeting["date"] = parser.parse(meeting["dagsetning"],
+                                   icelandic_dateutil_parserinfo)
+    data.append(meeting)
+scraperwiki.sqlite.save(unique_keys=['url'],
+                        data=data)
